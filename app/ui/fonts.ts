23d8a80de2c6b504355cb02b87bfa98678366fb4
cbmanu <carrero.besson@gmail.com>
2025-09-20 16:11:48 -0400

    nunita font

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter } from "next/font/google";
+import { Nunito_Sans } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const nunito_sans = Nunito_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
